745819b127af704cf0937cc9126d668db9a00aca
clayton14 <claytoneasley77@gmail.com>
2023-03-18 03:43:07 -0400

    working

--- src/ha_sockets.py
+++ src/ha_sockets.py
@@ -5,8 +5,8 @@ import os
 import sys
 import dotenv
 from termcolor import cprint
-
-
+import random
+import time
 dotenv.load_dotenv()
 
 base_url = os.getenv("SERVER_ENDPOINT")
@@ -15,53 +15,63 @@ token = os.getenv("TOKEN")
 # token = "l"
 
 
-async def auth():
-    """
-        completes the server client auth handshake
-        returns: websocket session
-    """
+async def start():
     try:
         async with websockets.connect(base_url) as ws:
-            message = await ws.recv()
-            message = await ws.send(json.dumps({
-                "type": "auth",
-                "access_token": token
-            }))
-            auth_status = await ws.recv()
-            auth_status = json.loads(auth_status)
-            if auth_status["type"] == "auth_invalid":
-                cprint(
-                    "[ERROR] Invalid auth token! Please generate an auth token in Home Assistant", "red")
-            if auth_status["type"] == "auth_ok":
-                cprint("=> Auth succuss", "green")
+            data = {
+                "id": 0,
+                "type": "call_service",
+                "domain": "light",
+                "service": "turn_on",
+                "service_data": {
+                    "hs_color": [300, 100],
+                    "brightness": 250
+                },
+                "target": {
+                    "entity_id": "light.sofa_light"
+                }
+            }
+
+            
+
+            await auth(ws)
+            while 1:
+                time.sleep(0.1)
+                data["id"] += 1
+                data["service_data"]["hs_color"][0] = random.randint(0, 360)
+                print(data["service_data"]["hs_color"][0])
+                await send_command(ws, data)
+
     except (TimeoutError, OSError):
         cprint(
             "[CONNECTION ERROR] Make sure you are connected to the correct network", "yellow")
         return None
-    
-    return ws
+
+async def auth(ws):
+    """
+        completes the server client auth handshake
+        returns: websocket session
+    """
+    message = await ws.recv()
+    message = await ws.send(json.dumps({
+        "type": "auth",
+        "access_token": token
+    }))
+    auth_status = await ws.recv()
+    auth_status = json.loads(auth_status)
+    if auth_status["type"] == "auth_invalid":
+        cprint(
+            "[ERROR] Invalid auth token! Please generate an auth token in Home Assistant", "red")
+    if auth_status["type"] == "auth_ok":
+        cprint("=> Auth succuss", "green")
 
 
-data = json.dumps({
-    "id": 24,
-    "type": "call_service",
-    "domain": "light",
-    "service": "turn_on",
-    "service_data": {
-        "color_name": "beige",
-        "brightness": "100"
-    },
-    "target": {
-        "entity_id": "sofa.light"
-    }
-})
+async def send_command(ws, data):
+    command = await ws.send(json.dumps(data))
+    #print(command)
+    stat = await ws.recv()
+    #print(stat)
+
+asyncio.run(start())
 
 
-async def send_command(session, data):
-    comand = await session.send(data)
-    print(comand)
-    stat = await session.recv()
-    print(stat)
-
-ws = asyncio.run(auth())
-asyncio.run(send_command(ws, data))
