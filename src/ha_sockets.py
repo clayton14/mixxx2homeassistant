c66493b4c98bceeb612e62ca20755b94108bd33f
clayton14 <claytoneasley77@gmail.com>
2023-03-18 02:11:09 -0400

    auth status

--- src/ha_sockets.py
+++ src/ha_sockets.py
@@ -1,5 +1,5 @@
 import asyncio
-import socket
+import websockets
 import json
 import os, sys, dotenv
 
@@ -7,8 +7,25 @@ dotenv.load_dotenv()
 
 base_url = os.getenv("SERVER_ENDPOINT")
 token = os.getenv("TOKEN")
+#print(f"URL -> {base_url}\nTOKEN -> {token}")
 
-print(f"URL -> {base_url}\nTOKEN -> {token}")
+async def auth():
+    try:
+        async with websockets.connect(base_url) as ws:
+            message = await ws.recv()
+            print(message)
+            auth_status = await ws.send(json.dumps({
+                "type" : "auth",
+                "access_token": token
+            }))
+            print(auth_status)
+            message = await ws.recv()
 
-async def listen():
-    pass 
+    except ConnectionError as e:
+        print("ERROR")
+        print(e) # this is dumb    
+
+    print(message)
+
+
+asyncio.run(auth())
