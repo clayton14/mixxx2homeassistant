fe9ebbc0cce0139d15236197d439cd302a08135f
clayton14 <claytoneasley77@gmail.com>
2023-03-17 23:42:26 -0400

    update

--- scr/midi_audio_visuliser.py
+++ scr/midi_audio_visuliser.py
@@ -6,13 +6,11 @@ class MIDI_AudioVisualizer():
     Python class that takes midi device as an input and returns rgb
     """
 
-
     def __init__(self, midi_device_port: str):
         self.port = midi_device_port()
+        self.bpm = 0
 
-
-
-    def send_message(self):
+    def send_message(self, ):
         pass
 
 
@@ -23,5 +21,8 @@ class MIDI_AudioVisualizer():
     def read_midi_file(self, file:str):
         pass
 
+
+    def recieve_message(self, ):
+        pass
 
 
