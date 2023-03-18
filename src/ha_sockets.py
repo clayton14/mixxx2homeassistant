ef848039c150a642888ad40086ef756f157f1a8e
clayton14 <claytoneasley77@gmail.com>
2023-03-18 02:19:17 -0400

    colors and auth flow

--- src/ha_sockets.py
+++ src/ha_sockets.py
@@ -1,31 +1,39 @@
 import asyncio
 import websockets
 import json
-import os, sys, dotenv
+import os
+import sys
+import dotenv
+from termcolor import cprint
+
 
 dotenv.load_dotenv()
 
 base_url = os.getenv("SERVER_ENDPOINT")
-token = os.getenv("TOKEN")
-#print(f"URL -> {base_url}\nTOKEN -> {token}")
+# token = os.getenv("TOKEN")
+# print(f"URL -> {base_url}\nTOKEN -> {token}")
+token = "l"
+
 
 async def auth():
     try:
         async with websockets.connect(base_url) as ws:
             message = await ws.recv()
             print(message)
-            auth_status = await ws.send(json.dumps({
-                "type" : "auth",
+            message = await ws.send(json.dumps({
+                "type": "auth",
                 "access_token": token
             }))
+            auth_status = await ws.recv()
+            auth_status = json.loads(auth_status)
             print(auth_status)
-            message = await ws.recv()
+            if auth_status["type"] == "auth_invalid":
+                cprint(
+                    "[ERROR] Invalid auth token! Please genrate an auth toke in Home Assistant", "red")
 
     except ConnectionError as e:
         print("ERROR")
-        print(e) # this is dumb    
-
-    print(message)
+        print(e)  # this is dumb
 
 
 asyncio.run(auth())
