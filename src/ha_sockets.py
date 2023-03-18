720f9b5f4722bcbaa29ccb241cac80189bf43a63
clayton14 <claytoneasley77@gmail.com>
2023-03-18 03:03:00 -0400

    error handling

--- src/ha_sockets.py
+++ src/ha_sockets.py
@@ -10,30 +10,58 @@ from termcolor import cprint
 dotenv.load_dotenv()
 
 base_url = os.getenv("SERVER_ENDPOINT")
-# token = os.getenv("TOKEN")
+token = os.getenv("TOKEN")
 # print(f"URL -> {base_url}\nTOKEN -> {token}")
-token = "l"
+# token = "l"
 
 
 async def auth():
+    """
+        completes the server client auth handshake
+        returns: websocket session
+    """
     try:
         async with websockets.connect(base_url) as ws:
             message = await ws.recv()
-            print(message)
             message = await ws.send(json.dumps({
                 "type": "auth",
                 "access_token": token
             }))
             auth_status = await ws.recv()
             auth_status = json.loads(auth_status)
-            print(auth_status)
             if auth_status["type"] == "auth_invalid":
                 cprint(
-                    "[ERROR] Invalid auth token! Please genrate an auth toke in Home Assistant", "red")
+                    "[ERROR] Invalid auth token! Please generate an auth token in Home Assistant", "red")
+            if auth_status["type"] == "auth_ok":
+                cprint("=> Auth succuss", "green")
+    except (TimeoutError, OSError):
+        cprint(
+            "[CONNECTION ERROR] Make sure you are connected to the correct network", "yellow")
+        return None
+    
+    return ws
 
-    except ConnectionError as e:
-        print("ERROR")
-        print(e)  # this is dumb
+
+data = json.dumps({
+    "id": 24,
+    "type": "call_service",
+    "domain": "light",
+    "service": "turn_on",
+    "service_data": {
+        "color_name": "beige",
+        "brightness": "100"
+    },
+    "target": {
+        "entity_id": "sofa.light"
+    }
+})
 
 
-asyncio.run(auth())
+async def send_command(session, data):
+    comand = await session.send(data)
+    print(comand)
+    stat = await session.recv()
+    print(stat)
+
+ws = asyncio.run(auth())
+asyncio.run(send_command(ws, data))
